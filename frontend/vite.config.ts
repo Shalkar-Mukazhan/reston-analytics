9a9875544beedbf660f4155f6dd92f05fde66610
Shalkar <shalkar@MacBook-Pro-Shalkar.local>
2026-04-17 13:22:42 +0500

    vite: allow all hosts for production server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://backend:8000',
